d74bde91302d75a3c6bb67c61466372b2ffabc4b
Antoine Cotten <hello@acotten.com>
2017-09-28 17:17:21 +0200

    Use OpenSSL package from CentOS 7 (1708) repo

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -10,7 +10,7 @@ fi
 versions=( "${versions[@]%/}" )
 
 nghttp2VersionDebian="$(docker run -i --rm centos:7 bash -c 'yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm && yum info "$@"' -- 'nghttp2' |tac|tac| awk -F ': ' '$1 ~ /^Version/ { print $2; exit }')"
-opensslVersionDebian="$(docker run -i --rm centos:7 bash -c 'yum install -y https://downloads.ulyaoth.net/rpm/ulyaoth-latest.centos.x86_64.rpm && yum info "$@"' -- 'ulyaoth-openssl1.0.2' |tac|tac| awk -F ': ' '$1 ~ /^Version/ { print $2; exit }')"
+opensslVersionDebian="$(docker run -i --rm centos:7 bash -c 'yum info "$@"' -- 'openssl' |tac|tac| awk -F ': ' '$1 ~ /^Version/ { print $2; exit }')"
 
 for version in "${versions[@]}"; do
 	fullVersion="$(curl -sSL --compressed "https://www.apache.org/dist/httpd/" | grep -E '<a href="httpd-'"$version"'[^"-]+.tar.bz2"' | sed -r 's!.*<a href="httpd-([^"-]+).tar.bz2".*!\1!' | sort -V | tail -1)"
